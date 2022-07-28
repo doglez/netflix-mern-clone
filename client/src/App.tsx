5fd388d72bd0185e35a3bf8c346f89000a3f4fa9
doglez <danilo.j.gonzalez@gmail.com>
2022-07-28 14:17:09 -0600

    create routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,47 @@
 import React from "react";
 import SignUp from "./pages/auth/SignUp";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/auth/SignIn";
+import Home from "./pages/home/Home";
 
 function App() {
+    const token: string = "";
+
     return (
-        <div>
-            <SignUp />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                {!token ? (
+                    <Route>
+                        <Route
+                            path="/"
+                            element={<Navigate to="/signup" replace />}
+                        />
+                        <Route path="/">
+                            <Route path="signup" element={<SignUp />} />
+                            <Route path="signin" element={<SignIn />} />
+                        </Route>
+                        <Route
+                            path="*"
+                            element={<Navigate to="/signup" replace />}
+                        />
+                    </Route>
+                ) : (
+                    <Route>
+                        <Route
+                            path="/"
+                            element={<Navigate to="/home" replace />}
+                        />
+                        <Route path="/">
+                            <Route path="home" element={<Home />} />
+                        </Route>
+                        <Route
+                            path="*"
+                            element={<Navigate to="/home" replace />}
+                        />
+                    </Route>
+                )}
+            </Routes>
+        </BrowserRouter>
     );
 }
 
