827a0cd039f0939404363356ace3bd4a572a366a
doglez <danilo.j.gonzalez@gmail.com>
2022-09-03 21:31:42 -0600

    user model

---
+++ server/src/models/User.ts
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRE, JWT_SECRET } from "../config/Config";
+import crypto from "crypto";
+
+export interface IUser {
+    name: string;
+    lastName: string;
+    email: string;
+    password: string;
+    resetPasswordToken: string;
+    resetPasswordExpire: Date;
+    getSignedJwtToken(): Function;
+    matchPassword(password: string): Function;
+    getResetPasswordToken(): Function;
+}
+
+const UserSchema = new mongoose.Schema<IUser>(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required."],
+            trim: true,
+            maxlength: [50, "Name can not be more than 50 characters."],
+            minlength: [3, "Name must be at least 3 characters."],
+        },
+        email: {
+            type: String,
+            required: [true, "Please add an email"],
+            unique: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please add a valid email",
+            ],
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            select: false,
+        },
+        resetPasswordToken: String,
+        resetPasswordExpire: Date,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Standarization for name uppercase the first character of each word
+UserSchema.pre("save", function () {
+    const splitStr = this.name.toLowerCase().split(" ");
+
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] =
+            splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+
+    this.name = splitStr.join(" ");
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this.id }, JWT_SECRET, { expiresIn: JWT_EXPIRE });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = function (enteredPassword: string) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    // Has token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
+
+export default mongoose.model("User", UserSchema);
