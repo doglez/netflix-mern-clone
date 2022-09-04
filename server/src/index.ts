5d3f98a249faa55e2891dfcefdfbf236d10b79c7
doglez <danilo.j.gonzalez@gmail.com>
2022-09-03 21:30:14 -0600

    Initial config

---
+++ server/src/index.ts
@@ -0,0 +1,93 @@
+import colors from "colors";
+import cookieParser from "cookie-parser";
+import express, { Express } from "express";
+import morgan from "morgan";
+import {
+    CORS_ADMIT_URL,
+    MAX_FILE_UPLOAD,
+    NODE_ENV,
+    PORT,
+} from "./config/Config";
+import ConnectDB from "./database/ConnectDB";
+import fileUpload from "express-fileupload";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
+import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
+import hpp from "hpp";
+import cors from "cors";
+import ErrorHandler from "./middleware/ErrorHandler";
+import Routes from "./routes/Routes";
+
+colors.enable();
+
+ConnectDB();
+
+const app: Express = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev loggin middleware
+if (NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// File uploading
+app.use(
+    fileUpload({
+        limits: { fieldSize: MAX_FILE_UPLOAD },
+        createParentPath: true,
+        safeFileNames: true,
+        preserveExtension: true,
+        abortOnLimit: true,
+    })
+);
+
+// Sanitize data prevent NoSQL injection && sanitize data
+app.use(
+    ExpressMongoSanitize({
+        replaceWith: "_",
+    })
+);
+
+// Set security header with helmet
+app.use(helmet({ crossOriginResourcePolicy: false }));
+
+// Rate limit
+const limmiter: RateLimitRequestHandler = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 1000000,
+});
+app.use(limmiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(
+    cors({
+        origin: CORS_ADMIT_URL,
+        credentials: true,
+        optionsSuccessStatus: 200,
+    })
+);
+
+app.use("/api/v1", Routes);
+
+app.use(ErrorHandler);
+
+const server = app.listen(PORT, () => {
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
+            .yellow.bold
+    );
+});
+
+// Handle unhandle promise rejection
+process.on("unhandledRejection", (reason: Error) => {
+    console.log(`Error: ${reason}`.bgRed);
+    server.close(() => process.exit(1));
+});
