592ba0d41d19ed0ff08b6cf0ab2eafa13ce50c50
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-09-19 07:54:44 -0600

    Home set for url

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <>
+            <Outlet />
+        </>
+    );
 };
 
 export default Home;
