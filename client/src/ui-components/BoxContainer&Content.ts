7aa1730c2e92c630556722774a4c86544b9a4a67
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-04 20:33:20 -0600

    show content by size

--- client/src/ui-components/BoxContainer&Content.ts
+++ client/src/ui-components/BoxContainer&Content.ts
@@ -18,3 +18,17 @@ export const SignContentBox = styled(Box)(({ theme }) => ({
         margin: "40px 40px",
     },
 }));
+
+export const HiddenDown: any = styled("div")(({ theme }) => (props: any) => ({
+    display: "block",
+    [theme.breakpoints.down(props.size)]: {
+        display: "none",
+    },
+}));
+
+export const DisplayUp: any = styled("div")(({ theme }) => (props: any) => ({
+    display: "block",
+    [theme.breakpoints.up(props.size)]: {
+        display: "none",
+    },
+}));
