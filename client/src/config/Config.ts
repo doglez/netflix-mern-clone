56cbe1338a0579fc4219b13c47db54148e2fb92e
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-11-15 11:03:54 -0600

    TMDB variables

--- client/src/config/Config.ts
+++ client/src/config/Config.ts
@@ -1,3 +1,5 @@
-export const API_URL_SERVER: string = String(
-    process.env.REACT_APP_API_URL_SERVER
-);
+export const API_URL_SERVER = String(process.env.REACT_APP_API_URL_SERVER);
+
+export const KEY_TMDB = String(process.env.REACT_APP_API_KEY_TMDB);
+
+export const TMDB_URL = String(process.env.REACT_APP_TMDB_URL_SERVER);
