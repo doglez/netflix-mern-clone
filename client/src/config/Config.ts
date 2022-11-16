9dfb3c3ab5dac15412a71b296b063ac20e748c50
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-11-16 08:20:27 -0600

    imagen url for tmdb

--- client/src/config/Config.ts
+++ client/src/config/Config.ts
@@ -1,5 +1,4 @@
 export const API_URL_SERVER = String(process.env.REACT_APP_API_URL_SERVER);
-
 export const TOKEN_TMDB = String(process.env.REACT_APP_API_KEY_TMDB);
-
 export const TMDB_URL = String(process.env.REACT_APP_TMDB_URL_SERVER);
+export const IMG_TMDB = String(process.env.REACT_APP_IMG_TMDB_URL);
