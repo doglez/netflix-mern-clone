0d680b54f568513af7889088c1bd0cc2f7f7ea65
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-11-22 10:55:15 -0600

    video url

--- client/src/config/Config.ts
+++ client/src/config/Config.ts
@@ -2,3 +2,4 @@ export const API_URL_SERVER = String(process.env.REACT_APP_API_URL_SERVER);
 export const TOKEN_TMDB = String(process.env.REACT_APP_API_KEY_TMDB);
 export const TMDB_URL = String(process.env.REACT_APP_TMDB_URL_SERVER);
 export const IMG_TMDB = String(process.env.REACT_APP_IMG_TMDB_URL);
+export const VIDEO_YOUTUBE = String(process.env.REACT_APP_VIDEO_YOUTUBE);
