b562fd260634952efe550d412e620feb875f3874
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-11-29 19:14:15 -0600

    simple usage

--- server/src/index.ts
+++ server/src/index.ts
@@ -67,23 +67,7 @@ app.use(limmiter);
 app.use(hpp());
 
 // Enable CORS
-const allowedOrigins = ["http://localhost:3000", CORS_ADMIT_URL];
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin
-            // (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                let msg =
-                    "The CORS policy for this site does not " +
-                    "allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-    })
-);
+app.use(cors());
 
 app.use(express.static(__dirname + "/public"));
 app.use("/api/v1", Routes);
